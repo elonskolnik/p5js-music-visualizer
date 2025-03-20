ba6d2a4e3bca2aad57617ae4b4709f8bb7456f13
elonskolnik <42852166+elonskolnik@users.noreply.github.com>
2025-03-20 14:07:42 -0700

    Newest version implementing basic Launchkey encoder control

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 //<script src="https://cdn.jsdelivr.net/gh/elonskolnik/p5js-music-visualizer@main/main.js 
 var noteOn = false;
-var noteNum = 1;
 var note = "";
 var noteY = 0;
 var noteColor = "white";
-var velocity = 0;
+
+var midimsgtype = 0;
+var midimsg = 1;
+var midivelocity = 0;
+var enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8 = 0;
+
 const numKeys = 76;
 var keyboard;
 const s1name = "S-1"; 
-const keyboardName = "CASIO USB-MIDI";
+const casioName = "CASIO USB-MIDI";
+const launchkeyName = "Launchkey Mini MK4 37 MIDI";
+
 var audioIn, lvl;
 const canvasX = 1180;
 const canvasY = 820;
@@ -44,11 +50,11 @@ function onMIDISuccess(midiAccess) {
       	console.log('No MIDI devices found');
       } else{
       		console.log(`${input.name} discovered!`);
-          document.body.innerHTML = `${input.name} discovered!`;
-          if (input.name == keyboardName || input.name == s1name){
+          //document.body.innerHTML = `${input.name} discovered!`;
+          if (input.name == casioName || input.name == s1name || input.name == 			 launchkeyName){
             keyboard = input;
           } else{
-            console.log(keyboardName + " not found!");
+            console.log("Keyboard not found!");
           }
       }
       /*input.onmidimessage = (message) => {
@@ -71,19 +77,61 @@ function onMIDIFailure() {
 function getMIDImessage(msg){
 	//console.log(msg.data);
   //document.body.innerHTML= 'MIDI message received after call';
-  if(msg.data[0] == 144){
+  midimsgtype = msg.data[0];
+  midimsg = msg.data[1];
+  midivelocity = msg.data[2];
+    
+  if(midimsgtype == 144 && midivelocity > 0){
   	noteOn = true;
+    midimsg -= 15;
+    setNote(midimsg);
   } else noteOn = false;
-  noteNum = msg.data[1] - 15;
-  setNote(noteNum);
-  
-  velocity = msg.data[2];
   if(noteOn){
- 		console.log("Note " + note + " " + noteNum + " on at " + velocity + " velocity\n");
+ 		console.log("Note " + note + " " + midimsg + " on at " + midivelocity + " velocity\n");
     //document.body.innerHTML= `Note ${note} ${noteNum} on at ${velocity} velocity`;
-  } else if(!Number.isNaN(noteNum)){
-  	console.log("Note " + note + " " + noteNum + " off\n");
+  /*} else if(!Number.isNaN(midimsg)){
+  	console.log("Note " + note + " " + midimsg + " off\n");*/
   }
+  /*if(midimsg != undefined){
+  	console.log(`MIDI Type: ${midimsgtype}\n Message: ${midimsg}\n Velocity: ${midivelocity}`);
+  }*/
+  
+  if(midimsgtype == 179){
+  	setEncoder(midimsg, midivelocity);
+  }
+}
+
+function setEncoder(midimsg, midivelocity){
+	switch(midimsg){
+  	//21-36
+  	case 21:
+    	enc1 = midivelocity;
+      break;
+    case 22:
+    	enc2 = midivelocity;
+      break;
+    case 23: 
+    	enc3 = midivelocity;
+      break;
+    case 24:
+    	enc4 = midivelocity;
+      break;
+    case 25:
+    	enc5 = midivelocity;
+      break;
+    case 26: 
+    	enc6 = midivelocity;
+      break;
+    case 27:
+    	enc7 = midivelocity;
+      break;
+    case 28:
+    	enc8 = midivelocity;
+      break;
+    default:
+    	console.log("Encoder " + midimsg + " not programmed");
+  }
+  console.log(`Encoder ${midimsg-20} set to ${midivelocity}`);
 }
 
 function setNote(noteNum){
@@ -119,8 +167,6 @@ function setNote(noteNum){
     case 5:
    		note = 'G#';
       noteY = interval;
-      noteColor = 'gold';
-      break;
     case 6:
     	note = 'A';
       noteY = interval * 12;
@@ -164,7 +210,7 @@ var numBands = 32;
 var waveBands = 2048;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   colorMode(HSB);
   angleMode(DEGREES);
   smooth();
@@ -178,6 +224,7 @@ function setup() {
   strokeWeight(2);
   noFill();
   stroke('black');
+  noteColor = 'gold';
   background('black');
   
   getAudioContext().suspend();
@@ -211,17 +258,36 @@ function draw() {
   /*if(frameCount % 1000 == 0){
   	background('black');
   }*/
-  background('black');
+  // Enable orbiting with the mouse.
+  orbitControl();
+
+  // Turn on the lights.
+  ambientLight(255);
+  directionalLight(255, 255, 255, 0, 0, -1);
+  
+  background(0, 5, 5);
   if(mic.getLevel() > 0){
   
     //amplitude
     var spectrum = ampfft.analyze();
+    var amp, sqamp, shapeY;
+    var wavehue, wavesat, waveb;
+    var enchue;
+    
     for(var i = spectrum.length; i > 0; i--){
-			var amp = spectrum[i];
-			var sqamp = Math.sqrt(amp);
-      var shapeY = map(sqamp, 0, 16, height, 0);
-      noStroke();
-      fill(amp, 128, 128);
+			amp = spectrum[i];
+      wavehue = map(amp, 0, 255, 0, 360);
+      
+      enchue = wavehue + enc1;
+      if(enchue <= 360){
+      	wavehue = enchue;
+      } else{
+      	wavehue = enchue - 360;
+      }
+      
+      //sqamp = Math.sqrt(amp);
+      //shapeY = map(sqamp, 0, 16, height, 0);
+      //console.log(amp);
       //rect(i*ampW, shapeY+40, ampW, height-shapeY);
     }
     
@@ -230,7 +296,7 @@ function draw() {
     strokeWeight(9);
     //stroke('hotpink');
     noFill();
-    beginShape();
+    beginShape(QUAD_STRIP);
     
     var negtotal = 0;
     var postotal = 0;
@@ -238,14 +304,29 @@ function draw() {
     var poscount = 0;
     
     for(var i = 0; i < wavespectrum.length; i++){     
-    	var wavehue = map(poscount, 800, 1200, 0, 360);
-      var wavesat = map(negcount, 800, 1200, 30, 100);
-      var waveb = map(poscount, 800, 1200, 30, 100);
-    	stroke(wavehue, wavesat, waveb);
+      wavesat = map(negcount, 800, 1200, 85, 100);
+      waveb = map(poscount, 800, 1200, 85, 100);
+    	//stroke(wavehue, wavesat, waveb);
+      //fill(wavehue, wavesat, waveb);
       
-			var waveX = map(i, 0, wavespectrum.length, 0, width);
+			var waveX = map(i, 0, wavespectrum.length, -width*1.5, width*1.5);
       var waveY = map(wavespectrum[i], -1, 1, height*6, -height*6);
-     	vertex(waveX, waveY+height/2);
+     	var waveZ = map(amp, 60, 150, -200, 200); 
+      
+      if(frameCount % 2 == 0){
+      	wavehue = map(waveY, height, -height, 0, 360);
+        enchue = wavehue + enc2;
+        if(enchue <= 360){
+          wavehue = enchue;
+        } else{
+          wavehue = enchue - 360;
+        }
+      }
+      //wavehue = map(negcount, 800, 1200, 20, 340);
+      stroke(wavehue, wavesat, waveb);
+      let endPoint = createVector(0, 20);
+    	endPoint.rotate((endPoint.z + frameCount) * 0.1);
+      vertex(waveX, waveY, endPoint.z);
       
       if(wavespectrum[i] < 0){
       	negtotal += wavespectrum[i];
@@ -265,12 +346,12 @@ function draw() {
     negtotal /= negcount;
     postotal /= poscount;
     //console.log(`Neg: ${negtotal} -- Pos: ${postotal}`);
-    console.log(`poscount = ${poscount}\n negcount = ${negcount}`);
+    //console.log(`poscount = ${poscount}\n negcount = ${negcount}`);
   }
   if(noteOn){
-  	circleX = map(noteNum, 0, 88, 150, width-150);
-    circleY = map(noteNum, 0, 88, 150, height-150);
-    velocitysize = map(velocity, 1, 127, 20, 200);
+  	circleX = map(midimsg, 0, 88, 150, width-150);
+    circleY = map(midimsg, 0, 88, 150, height-150);
+    velocitysize = map(midivelocity, 1, 127, 20, 200);
   	fill(noteColor);
   	circle(circleX, noteY, velocitysize);
     fill('white');
